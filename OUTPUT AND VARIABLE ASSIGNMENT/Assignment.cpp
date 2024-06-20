8f94692aa0af6f83e0c1ff13772f94a0e1749ec1
Jigyasu90 <jigyasusolanki195@gmail.com>
2024-06-20 07:14:06 -0700

    Add files via upload
    OUTPUT AND VARIABLE ASSIGNMENT ANSWER
    If I  have uploaded assignment in a wrong way. please let me know.
    Because I am not able understand how to upload assignments.

---
+++ OUTPUT AND VARIABLE ASSIGNMENT/Assignment.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+//Q1
+int main(){
+    cout<<"Physics "<<endl;
+    cout<<"Wallah ";
+
+}
+
+//Q2
+int main(){
+    int a,b;
+    a=4;
+    b=6;
+    cout<<a+b;
+}
+
+//Q4
+int main(){
+    int a=4;
+    int b=5;
+    a++;b--;
+    cout<<++a<<" "<<b--;
+}
+
+//Q5
+int main(){
+    int r =10;
+    float pi = 3.1415;
+
+    cout<<"Circumference = "<<2*pi*r;
+}
