4dfaf794f705f9625321da6add7dfe3e63a15609
Jigyasu90 <jigyasusolanki195@gmail.com>
2024-07-06 22:20:23 +0530

    Add files via upload

---
+++ C++ Assignments  Loops 1/ASSINGMENT.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+// Q1
+int main(){
+    for(int i=1;i<=100;i+=2){
+        cout<<i<<'\t';
+    }
+}
+
+// // Q2
+// int main(){
+//     for(int i=1;i<=100;i+=2){
+//         if(i%3==0){
+//             cout<<i<<'\t';
+//         }
+//     }
+// }
+
+// // Q3
+// int main(){
+//     int n;
+//     cout<<"Enter the number : ";
+//     cin>>n;
+
+//     for(int i=1;i<=10;i++){
+//         cout<<n<<" X "<<i<<" = "<<n*i<<endl;
+//     }
+// }
+
+// // Q4
+// int main(){
+//     int n;
+//     cout<<"Enter the number of terms : ";
+//     cin>>n;
+
+//     int a =4;
+//     for(int i=1;i<=n;i++){
+//         cout<<a<<endl;
+//         a+=3;
+//     }
+// }
+
+// // Q5
+// int main(){
+//     int n;
+//     cout<<"Enter the number of terms : ";
+//     cin>>n;
+
+//     int a =3;
+//     for(int i=1;i<=n;i++){
+//         cout<<a<<endl;
+//         a*=4;
+//     }
+// }
+
+// // Q6
+// int main(){
+//     cout<<"ASCII value <--->Character"<<endl;
+//     for (int i=65;i<=90;i++){
+
+//         cout<<i<<'\t'<<(char)i<<endl;
+//     }
+// }
